6894812080924dda890d2615e7cf1f65266cb7ba
endri <lickollariendri@gmail.com>
2022-10-07 22:17:55 +0200

    api key updated

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '...',
+        'X-RapidAPI-Key': '8d81655bddmsh55949fa77c73957p161018jsn9847333c359a',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "...";
+export const WEATHER_API_KEY = "9842686703e0bdd84db174da3c8dcc28";
